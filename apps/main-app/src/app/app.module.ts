81cdb8876d0cf8a4fc00da40c2336d2217f07d91
Gerald Chifanzwa <gchifanzwa@zss.co.zw>
2023-04-24 09:43:42 +0200

    descriptions

--- apps/main-app/src/app/app.module.ts
+++ apps/main-app/src/app/app.module.ts
@@ -4,9 +4,24 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+/* These 2 components were generated using the commands:
+   - nx generate @nrwl/angular:component component-in-root --project main-app --style scss
+   - nx g component my-component --project main-app --style scss
+     --> select @nrwl/angular:component from the list that is shown.
+*/
 import { ComponentInRootComponent } from './component-in-root/component-in-root/component-in-root.component';
 import { MyComponentComponent } from './my-component/my-component/my-component.component';
+
+/*
+  This component was generated via:
+  - nx g component my-component2 --project main-app --style scss
+    --> select @schematics/angular:component from the list that is shown
+*/
 import { MyComponent2Component } from './my-component2/my-component2.component';
+/*
+ * Components generated using @nrwl/angular:component have the component directory duplicated in the path.
+ */
+
 
 @NgModule({
   declarations: [
